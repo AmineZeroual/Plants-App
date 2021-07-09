261861efa8fd0d1bae647adfe8856204562dd675
Amine Zeroual <73144385+AmineZeroual@users.noreply.github.com>
2021-07-09 17:01:38 +0100

    BasicsChanges
    Creating the first Page

---
+++ lib/View/MainPage.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+
+
+class MainPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [
+          Container(
+            height: 200,
+            width: 100,
+            color: Colors.amber,
+          )
+        ],
+      ),
+    );
+  }
+}
